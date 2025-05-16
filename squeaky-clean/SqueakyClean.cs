b99497e85ee0fc89d2da8522382e8f8712a0a52f
schmensch <Manuel.B3@nurfuerspam.de>
2025-05-16 12:16:26 +0200

    exercise SqueakyClean done

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,33 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
         StringBuilder sb = new StringBuilder(identifier);
-        sb.replace(' ', '_');
-        sb.
+        MatchEvaluator evaluator = new MatchEvaluator(x => x.Value.Substring(1).ToUpper());
+        sb.Replace(' ', '_');
+        foreach (char c in sb.ToString())
+        {
+            if (Char.IsControl(c))
+            {
+                sb.Replace(c.ToString(), "CTRL");
+            }
+
+            if (!Char.IsLetter(c) && !(new[]{'_', '-'}.Contains(c)))
+            {
+                sb.Replace(c.ToString(), "");
+            }
+
+            if (Char.IsBetween(c, 'α', 'ω'))
+            {
+                sb.Replace(c.ToString(), "");
+            }
+        }
+
+        string solution = sb.ToString();
+        solution = Regex.Replace(solution, "-\\p{L}", evaluator, RegexOptions.None);
+        return solution;
     }
 }
