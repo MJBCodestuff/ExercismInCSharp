936be595ad901ada1ba5fb545ce0c8fd06bb104a
MJB <Manuel.J.Bauer@gmx.de>
2025-05-29 14:24:30 +0200

    new version of ScrabbleScore after trying some methods to fill the dict

--- scrabble-score/ScrabbleScore.cs
+++ scrabble-score/ScrabbleScore.cs
@@ -2,9 +2,32 @@ public static class ScrabbleScore
 {
     public static int Score(string input)
     {
-        Dictionary<string, int> scores = new Dictionary<string, int>();
-        
-
+        Dictionary<char, int> scores = new Dictionary<char, int>();
+        foreach (char c in "AEIOULNRST")
+        {
+            scores.Add(c, 1);
+        }
+        foreach (char c in "DG")
+        {
+            scores.Add(c, 2);
+        }
+        foreach (char c in "BCMP")
+        {
+            scores.Add(c, 3);
+        }
+        foreach (char c in "FHVWY")
+        {
+            scores.Add(c, 4);
+        }
+        scores.Add('K', 5);
+        foreach (char c in "JX")
+        {
+            scores.Add(c, 8);
+        }
+        foreach (char c in "QZ")
+        {
+            scores.Add(c, 10);
+        }
         return input.ToUpper().ToCharArray().Select(x => scores[x]).Sum();
     }
 }
